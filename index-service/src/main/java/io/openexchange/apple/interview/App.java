b9a1148f7d46daecb3cab4c637fc273271ac1ea7
andrew_kandzuba <andrey.kandzuba@gmail.com>
2020-03-09 07:17:23 -0500

    building project structure

---
+++ index-service/src/main/java/io/openexchange/apple/interview/App.java
@@ -0,0 +1,13 @@
+package io.openexchange.apple.interview;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
